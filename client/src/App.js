67e3a0215353f01b649721ef491d95bfa5a8e9e3
olupprince <99639095+olupprince@users.noreply.github.com>
2023-08-11 21:54:05 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Homepage from "./Homepage";
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "" : data}</p>
+      </header>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
