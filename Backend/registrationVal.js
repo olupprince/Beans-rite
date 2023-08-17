c48ca2e5ac0e17087f2a2c44918ef6dfd38ccaf7
Okunola Olayemi <Okunola.O@bullseye.local>
2023-08-17 23:47:57 +0100

    another commit

--- Backend/registrationVal.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi')
-
-const registrationSchema = Joi.object({
-  username: Joi.string().alphanum().min(4).max(30).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-});
-
-module.exports = registrationSchema;
