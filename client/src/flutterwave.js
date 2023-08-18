009d2ea64f8c1dcdef7d8e12103fe79d9f5c609a
hezzy1 <Oayeni88@gmail.com>
2023-08-18 14:28:45 +0100

    another commit

--- client/src/flutterwave.js
+++ client/src/flutterwave.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import beans from './img/beansLogo.png'
 import flutt from './img/flawt.jpeg'
 import './flutterwave.css'
@@ -8,6 +8,8 @@ import { FlutterWaveButton, closePaymentModal } from 'flutterwave-react-v3';
 
 
 const Flutterwave=(props) => {
+    const [amount, setAmount] = useState('')
+    const [name, set]
 
     const config = {
     public_key: 'FLWPUBK_TEST-25dcf4c3dcae9a5f4c62a009913585b0-X',
