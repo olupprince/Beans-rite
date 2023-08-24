c2259e8e2e9165571fa6c18100628cd5372b27f6
TheGodwinEbogu <godwinebogu@gmail.com>
2023-08-24 13:28:17 +0100

    commit with boyo and dami

--- client/src/components/Faq.js
+++ client/src/components/Faq.js
@@ -1,12 +1,7 @@
-import { useState } from 'react';
-import up from '../img/icon-up.png'
-import down from '../img/icon-down.png'
-import data from './FAQsData';
-import image from '../img/icon-faq.png';
-import '../styles/Ayeni.css';
-
-
-
+import { useState } from "react";
+import data from "./FAQsData";
+import image from "../img/faq-bg-img.jpeg";
+import "../styles/Ayeni.css";
 
 function FAQs() {
   const [selected, setSelected] = useState(null);
@@ -14,50 +9,51 @@ function FAQs() {
   const toggle = (i) => {
     if (selected === i) {
       return setSelected(null);
-    };
+    }
 
     setSelected(i);
-
-  }
+  };
 
   return (
-    <div className='containerxx'>
-        <img className="image" src={image} alt="FAQS"/>
-        <div className="wrapper">
-            <h2 className='faq'>FAQs</h2>
-            <div className='accordion'>
-
-                {data.map((item, i) => (
-                  <div className='item'>
-
-                  <span className='icon'>#</span>
-                  <div className='con'>
-                      <div className='title' onClick={() => toggle(i)}>
-                        <div className='quest'>
-                          <h2>{item.question}</h2>
-
-                          <span>{selected === i ?  
-                          <img className='img-icon' src={up} alt='icon'/>: 
-                          <img className='img-icon' src={down} alt='icon'/> 
-                          } </span>
-
-                        </div>
-                      </div>
-                      <div className='cont'>
-                        <div className={selected === i ? 'content show' : 'content'}>{item.answer}</div>
-                      </div>
-                    </div>
-                </div>
-                ))}
-
-            </div>
+    <section className="section-faq">
+      <div className="containerxx">
+        <div className="faq-img-box">
+          <img className="image" src={image} alt="FAQS" />
         </div>
-    </div>
+        <div className="wrapper">
+          <h2 className="faq">
+            {" "}
+            FAQs &nbsp;<span className="faq-icon"> 📙 </span>{" "}
+          </h2>
+          <div className="accordion">
+            {data.map((item, i) => (
+              <div className="item">
+                <span className="icon">#</span>
+                <div className="con">
+                  <div className="title" onClick={() => toggle(i)}>
+                    <div className="quest">
+                      <h2>{item.question}</h2>
+
+                      <span>
+                        {selected === i ? <span>❌</span> : <span> ⬇</span>}{" "}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="cont">
+                    <div
+                      className={selected === i ? "content show" : "content"}
+                    >
+                      {item.answer}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
-
-
-
-
 
 export default FAQs;
