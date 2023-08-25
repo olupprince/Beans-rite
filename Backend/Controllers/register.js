f0a37334038ac48f10d4babc6fc9d6ae30a05b0f
Oneluv1 <adegunju27@gmail.com>
2023-08-25 08:54:39 +0100

    new update

--- Backend/Controllers/register.js
+++ Backend/Controllers/register.js
@@ -1,39 +1,46 @@
-const schema = require('../validators/registrationVal')
-const User = require('../models/user')
-const bcrypt = require('bcrypt');
+const schema = require("../validators/registrationVal");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
-  const {username, password, email} = req.body
-  const userData = req.body
-  if (!username || !password || !email) {
-    res.json({message: 'Provide username, email and password'})
+  const { username, phone, password, email } = req.body;
+  const userData = req.body;
+  if (!username || !password || !email || !phone) {
+    res.json({ message: "Provide username, phone, email and password" });
   }
-   // Validate data using the Joi schema
+  if (!password || password.length < 6) {
+    return res
+      .status(400)
+      .json({ error: "Password must be at least 6 characters long" });
+  }
+  // Validate data using the Joi schema
   const { error } = schema.validate(userData);
   if (error) {
     const errorMessage = error.details[0].message;
     return res.status(400).json({ error: errorMessage });
   }
 
-   // check if user exists 
-  const existingUser = await User.findOne ({ email })
+  // check if user exists
+  const existingUser = await User.findOne({ email, phoneNumber });
   if (existingUser) {
-    return res.status(409).json({ error: 'Email is already registered' })
+    return res
+      .status(409)
+      .json({ error: "Email or Phone Number is already registered" });
   }
   // hash password
   const hashedPassword = await bcrypt.hash(password, 10);
 
   //create new user in the database
-  const newUser = new User ({
-    username, 
+  const newUser = new User({
+    username,
+    phoneNumber,
     email,
-    password: hashedPassword
+    password: hashedPassword,
   });
-    await newUser.save();
-  
-  // Return a success response
-  res.json({ success: true, message: 'Registration successful!' });
+  await newUser.save();
 
-}
+  // Return a success response
+  res.json({ success: true, message: "Registration successful!" });
+};
 
 module.exports = register;
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,25 +1,37 @@
 const mongoose = require("mongoose");
-const moment = require('moment-timezone')
+const moment = require("moment-timezone");
+const { number } = require("joi");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 50,
+    },
+    createdAt: {
+      type: Date,
+      default: moment().tz("Your_Timezone").add(1, "hours").toDate(),
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: moment().tz('Your_Timezone').add(1, 'hours').toDate(),
-  }
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
