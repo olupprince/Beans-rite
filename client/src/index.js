816903e05a03a57f4d24f7e036efa197008520e3
hezzy1 <Oayeni88@gmail.com>
2023-08-30 06:06:18 +0100

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { CartDev } from "../src/pages/CartContext";
+// import { CartDev } from "../src/pages/CartContext";
 // import { BrowserRouter } from "react-router-dom";
 import CartProvider from "./Hooks/useCart";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CartDev>
+    {/* <CartDev>
       <App />
-    </CartDev>
+    </CartDev> */}
     <CartProvider>
       <App />
     </CartProvider>
